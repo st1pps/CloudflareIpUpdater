c6f2eadb4da65a77bdefea875a94fcd0bd1e0bff
Fredrik Stipps <fredrik@stipps.net>
2023-04-03 13:06:26 +0200

    Added asp.net core API endpoint for fritzbox DynDns

---
+++ src/Stipps.CloudflareIpUpdater.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Stipps.CloudflareApi.Extensions.DependencyInjection;
+using Stipps.CloudflareIpUpdater.Configuration;
+using Stipps.CloudflareIpUpdater.Services;
+
+namespace Stipps.CloudflareIpUpdater.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCloudflareService(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHttpClient();
+        services.AddCloudflareApi(configuration);
+        
+        services.AddMemoryCache();
+            
+        services.AddOptions<CloudflareServiceSettings>()
+            .Configure(settings =>
+                configuration.GetSection(CloudflareServiceSettings.SectionName).Bind(settings))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+        services.AddScoped<CloudflareService>();
+        return services;
+    }
+}
